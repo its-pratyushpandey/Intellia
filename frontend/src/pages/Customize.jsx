d5b4ade40a433d6363cf348ec72650d84d1fabaf
Pratyush Kumar Pandey <2300030557@kluniversity.in>
2025-09-07 21:20:49 +0530

    Add import for image assets in Customize component

--- frontend/src/pages/Customize.jsx
+++ frontend/src/pages/Customize.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useRef, useState } from 'react'
 import Card from '../components/Card'
+
 import image1 from "../assets/image1.png"
 import image2 from "../assets/image2.jpg"
 import image3 from "../assets/authBg.png"
