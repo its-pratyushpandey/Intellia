fe30f576e7d9b5a2e8b19873dede69cbcf6b15aa
Pratyush Kumar Pandey <2300030557@kluniversity.in>
2025-09-09 23:35:20 +0530

    Remove unnecessary empty line in UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,6 +1,5 @@
 import axios from 'axios'
 import React, { createContext, useEffect, useState } from 'react'
-
 export const userDataContext = createContext()
 function UserContext({children}) {
     const serverUrl = "http://localhost:8000"
